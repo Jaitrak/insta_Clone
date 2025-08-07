7d86dfd57ec278a09dac4b5ca257665e12f9902a
Jaitrak <jaitrakpatel3824@gmail.com>
2025-08-07 20:31:14 +0530

    New Add

---
+++ routes/multer.js
@@ -0,0 +1,20 @@
+const multer = require("multer");
+const {v4: uuidv4} = require("uuid");
+const path = require("path");
+const crypto = require("crypto");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/images/uploads");
+  },
+  filename: function (req, file, cb) {
+    // const unique = uuidv4();
+    // cb(null, unique.extname(file.originalname));
+    const fn =
+      crypto.randomBytes(16).toString("hex") + path.extname(file.originalname);
+    cb(null, fn);
+  },
+});
+
+const upload = multer({ storage: storage });
+module.exports = upload;
